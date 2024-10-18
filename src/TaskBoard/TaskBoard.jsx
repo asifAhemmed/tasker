d8b05608f7c55cd07ca51cbc60081e1d8b809693
asif ahemmed <asifahemmed0@gmail.com>
2024-10-18 12:57:26 +0600

    add task functionality added

--- src/TaskBoard/TaskBoard.jsx
+++ src/TaskBoard/TaskBoard.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import SearchBox from "./SearchBox";
 import TaskActions from "./TaskActions";
 import TaskTable from "./TaskTable";
+import AddTaskModal from "./AddTaskModal";
 
 const TaskBoard = () => {
   const defaultTask = [
@@ -15,14 +16,21 @@ const TaskBoard = () => {
     }
   ]
   const [tasks, setTasks] = useState(defaultTask);
+  const [showModal, setShowModal] = useState(false);
+  const handleAddTask = ( newTask ) => {
+     setTasks([...tasks,newTask]);
+     setShowModal(false);
+  }
+
   return (
     <section className="mb-20" id="tasks">
+      {showModal && <AddTaskModal onClickSave={handleAddTask}></AddTaskModal>}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchBox></SearchBox>
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions></TaskActions>
+          <TaskActions onClickAddModal = {() => setShowModal(true)}></TaskActions>
           <div className="overflow-auto">
             <TaskTable tasks={tasks}></TaskTable>
           </div>
